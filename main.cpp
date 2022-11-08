e56ab4c2a9268ac01cdac21e0da0471a9e11ec47
bnies1017 <ben.nies1017@gmail.com>
2022-11-08 14:57:00 -0700

    update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,10 +1,10 @@
-#include "plotwindow.h"
+#include "mainwindow.h"
 #include <QApplication>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    PlotWindow w;
+    MainWindow w;
     w.show();
 
     return a.exec();
